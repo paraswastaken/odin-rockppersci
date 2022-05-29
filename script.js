26f125ef8f044449b5557268a77d5a894ea84101
paraswastaken <akarkaulia2000@gmail.com>
2022-05-29 23:06:45 +0530

    Add a simple UI and fix some JS logic

--- script.js
+++ script.js
@@ -7,32 +7,44 @@ let comp =0; player=0;
 
 function playRound(e) {
     cmp = computerP();
-    pl = `${e.target.getAttribute('class')}`.toString();
+    cmpsym = document.querySelector('#'+cmp).getAttribute('data-symbol');
+    pl = `${e.target.getAttribute('id')}`.toString();
     console.log(`Player: ${player}      Computer: ${comp}`);
     if(pl===cmp){
-        console.log(`Draw!! Both Computer and you had ${pl.toUpperCase()}`);
+        res.textContent = `Draw!! Both Computer and you had ${pl.toUpperCase()}`;
+        plmove.textContent = `${e.target.getAttribute('data-symbol')}`;
+        compmove.textContent = cmpsym;
         return;
     }
     if(pl==='rock'){
-        console.log(cmp==='paper'?`You Lose! ${cmp.toUpperCase()} beats ${pl.toUpperCase()}`:`You Win! ${pl.toUpperCase()} beats ${cmp.toUpperCase()}`);
+        res.textContent = cmp==='paper'?`You Lose! ${cmp.toUpperCase()} beats ${pl.toUpperCase()}`:`You Win! ${pl.toUpperCase()} beats ${cmp.toUpperCase()}`;
         cmp==='paper'?comp++:player++;
+        plmove.textContent = `${e.target.getAttribute('data-symbol')}`;
+        compmove.textContent = cmpsym;
         return;
     }
     if(pl==='paper'){
-        console.log(cmp==='scissors'?`You Lose! ${cmp.toUpperCase()} beats ${pl.toUpperCase()}`:`You Win! ${pl.toUpperCase()} beats ${cmp.toUpperCase()}`);
+        res.textContent = cmp==='scissors'?`You Lose! ${cmp.toUpperCase()} beats ${pl.toUpperCase()}`:`You Win! ${pl.toUpperCase()} beats ${cmp.toUpperCase()}`;
         cmp==='scissors'?comp++:player++;
+        plmove.textContent = `${e.target.getAttribute('data-symbol')}`;
+        compmove.textContent = cmpsym;
         return;
     }
     if(pl==='scissors'){
-        console.log(cmp==='rock'?`You Lose! ${cmp.toUpperCase()} beats ${pl.toUpperCase()}`:`You Win! ${pl.toUpperCase()} beats ${cmp.toUpperCase()}`);
+        res.textContent = cmp==='rock'?`You Lose! ${cmp.toUpperCase()} beats ${pl.toUpperCase()}`:`You Win! ${pl.toUpperCase()} beats ${cmp.toUpperCase()}`;
         cmp==='rock'?comp++:player++;
+        plmove.textContent = `${e.target.getAttribute('data-symbol')}`;
+        compmove.textContent = cmpsym;
         return;
     }
 }
 
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissors = document.querySelector(".scissors");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const res = document.querySelector('.results');
+const plmove = document.querySelector('.moves>.player');
+const compmove = document.querySelector('.moves>.computer');
 
 rock.addEventListener('click', playRound);
 paper.addEventListener('click', playRound);
