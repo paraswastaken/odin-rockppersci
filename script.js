753c98006b42170cece7f8dff551550c76e09381
Paras <98262496+paraswastaken@users.noreply.github.com>
2022-05-30 15:10:13 +0530

    Update script.js
    Added all necessary comments and fixed some clunky code.

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+// Function to randomly generate a string which is then used as computers move...
+//... in *playround()* function.
 function computerP() {
     randomNum = Math.random()
     return randomNum>0.67?'rock':randomNum>0.33?'paper':'scissors';
 }
 
-let comp =0; player=0;
+// Score counter Variables
+let comp = 0; player = 0;
 
+// Function to play a single round. It is called in the *click()* function...
+//...Argument passed is click data which is then used to reference target button.
 function playRound(e) {
-    cmp = computerP();
+    cmp = computerP();   // 
     cmpsym = document.querySelector('#'+cmp).getAttribute('data-symbol');
     pl = `${e.target.getAttribute('id')}`.toString();
     if(pl===cmp){
@@ -15,7 +20,7 @@ function playRound(e) {
         compmove.textContent = cmpsym;
         plscore.textContent = `Player: ${player}`;
         compscore.textContent = `Computer: ${comp}`;
-
+        
         return;
     }
     if(pl==='rock'){
@@ -50,15 +55,7 @@ function playRound(e) {
     }
 }
 
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
-const res = document.querySelector('.results');
-const plmove = document.querySelector('.moves>.player');
-const compmove = document.querySelector('.moves>.computer');
-const plscore = document.querySelector('.plsc');
-const compscore = document.querySelector('.compsc');
-
+// Function to handle click events on buttons.
 function click(e) {
     if(player===5||comp===5){
         reset();
@@ -70,6 +67,22 @@ function click(e) {
     }
 }
 
+// Query selectors for buttons used to make a move.
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+// Query selectors for editing text content of corresponding divs to display...
+//...result and scores after every round.
+const res = document.querySelector('.results');
+const plscore = document.querySelector('.plsc');
+const compscore = document.querySelector('.compsc');
+
+// Query selectors to display symbols of moves made by player and computer after every round.
+const plmove = document.querySelector('.moves>.player');
+const compmove = document.querySelector('.moves>.computer');
+
+// Function to reset scores after one BO5 ends.
 function reset(){
     player = 0; comp = 0;
     res.textContent = "Make Your first move!!";
@@ -77,6 +90,7 @@ function reset(){
     compscore.textContent = `Computer`;
 }
 
+// Event listener on all three buttons with *click* as callback function
 rock.addEventListener('click', click);
 paper.addEventListener('click', click);
 scissors.addEventListener('click', click);
